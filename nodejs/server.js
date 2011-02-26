624198887641ad7a7aa63a5be1be72e0d0ac7ab6
Henri Bergius <henri.bergius@iki.fi>
2011-02-26 18:25:05 +0200

    Make our RDFa persistent via Redis

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,7 +3,8 @@ var io = require('socket.io');
 var url = require('url');  
 var path = require('path'); 
 var fs = require('fs');
-var jQuery = require("jquery");
+var jQuery = require('jquery');
+var redis = require('redis');
 
 server = http.createServer(function(request, response){ 
     var uri = url.parse(request.url).pathname;
@@ -35,7 +36,14 @@ server = http.createServer(function(request, response){
     });  
 });
 server.listen(8002);
-  
+
+// REDIS
+var redisClient = redis.createClient();
+
+redisClient.on('error', function (err) {
+    console.log('Redis error ' + err);
+});
+
 // socket.io 
 var socket = io.listen(server);
 
@@ -46,16 +54,53 @@ socket.on('connection', function(client) {
     socketClients.push(client);
     console.log("New client " + client.identifier + " connected, client list is now", socketClients.length);
 
+    // Send current data from Redis
+    redisClient.hkeys('anon-ontologies', function(err, ontologies) {
+        ontologies.forEach(function(ontology, i) {
+            redisClient.lrange('anon-' + ontology, 0, -1, function(err, anonItems) {
+                anonItems.forEach(function(anonItem, i) {
+                    console.log(anonItem);
+                    client.send(JSON.parse(anonItem));
+                });
+            });
+        });
+    });
+    redisClient.hkeys('ontologies', function(err, ontologies) {
+        ontologies.forEach(function(ontology, i) {
+            redisClient.hkeys(ontology, function(err, itemIdentifiers) {
+                itemIdentifiers.forEach(function(itemIdentifier, i) {
+                    redisClient.hget(ontology, itemIdentifier, function(err, item) {
+                        console.log(item);
+                        client.send(JSON.parse(item));
+                    });
+                });
+            });
+        });
+    });
+
     client.on('message', function(data) {
         console.log("Got message from client " + client.identifier, data);
+        
+        if (typeof data === 'object') {
+            if (data['a'] !== undefined) {
+                if (data['@'] !== '<undefined>') {
+                    // Identified object, save to Redis hash based on type
+                    redisClient.hset(data['a'], data['@'], JSON.stringify(data));
+                    redisClient.hset('ontologies', data['a'], true);
+                } else {
+                    // Anonymous entity, save to Redis list based on type
+                    redisClient.lpush('anon-' + data['a'], JSON.stringify(data));
+                    redisClient.hset('anon-ontologies', data['a'], true);
+                }
+            }
+        }
 
         jQuery.each(socketClients, function(index, socketClient) {
             if (socketClient == client) {
-                
-                socketClient.send("Pong client " + socketClient.identifier);
                 return true;
             }
             console.log("Forwarding data to " + socketClient.identifier);
+
             socketClient.send(data);
         });
     });
